dec8ef9142570a1ab8d170565a1ed3314977ad42
Nitish <nitishoffica3@gmail.com>
2023-11-21 01:11:41 +0530

    NavBar-added

--- poll-app/src/App.js
+++ poll-app/src/App.js
@@ -1,45 +1,24 @@
-import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "./components/Home";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import userReducer from "./reducers/user-reducer";
 import { createContext, useReducer } from "react";
 import Dashboard from "./components/Dashboard";
-import _ from "lodash"
+
+import NavBar from "./components/Nav";
 
 export const userContext=createContext()
 
 function App() {
   const [state,dispatch]=useReducer(userReducer,{user:{}})
- 
-
-  const handleLogout=()=>{
-    localStorage.removeItem("token")
-    dispatch({type:"LOGOUT_USER"})
-    
-  }
 
   return (
   <BrowserRouter>
     <userContext.Provider value={{state,dispatch}}>
         <div >
           <h1>Polling App</h1>
-          <nav>
-            <li><Link to="/">Home</Link></li>   
-            {
-              _.isEmpty(state.user) ?(
-                <>
-                  <li><Link to="/register">Register</Link></li>
-                  <li><Link to="/login">Login</Link></li>
-                </>
-              ):(
-                <>
-                  <li><Link to="/dashboard">dashboard</Link></li>   
-                  <li><Link to="/logout" onClick={handleLogout}>logout</Link></li>
-                </>
-              )
-            } 
-          </nav>
+          <NavBar/>
         </div>
         <Routes> 
           <Route path="/" element={<Home/>} />
---
+++ poll-app/src/components/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { userContext } from '../App'
+import _ from "lodash"
+
+const NavBar = () => {
+
+    const {state,dispatch}=useContext(userContext)
+    const navigate=useNavigate()
+
+    const handleLogout=()=>{
+        localStorage.removeItem("token")
+        dispatch({type:"LOGOUT_USER"}) 
+        navigate("/")      
+      }
+  return (
+    <nav>
+            <li><Link to="/">Home</Link></li>   
+            {
+              _.isEmpty(state.user) ?(
+                <>
+                  <li><Link to="/register">Register</Link></li>
+                  <li><Link to="/login">Login</Link></li>
+                </>
+              ):(
+                <>
+                  <li><Link to="/dashboard">dashboard</Link></li> 
+                  <li><Link to="/" onClick={handleLogout}>logout</Link></li>
+                </>
+              )
+            } 
+          </nav>
+  )
+}
+
+export default NavBar

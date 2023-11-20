63193d8e2b877b04f1386f812125a6237610c31d
Nitish <nitishoffica3@gmail.com>
2023-11-20 22:13:41 +0530

    login Component

--- poll-app/src/components/Login.js
+++ poll-app/src/components/Login.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import React from 'react'
 import { useLocation } from "react-router-dom"
-
+import axios from  "axios"
+import  {useNavigate} from "react-router-dom"
 
 
 const Login = () => {
@@ -10,20 +11,45 @@ const Login = () => {
         password:"",
         email:""
     })
+    const [serverErrors,setSereverErrors]=useState([])
     const location=useLocation()
+    const navigate=useNavigate()
     const message=location.state?location.state.message : ""
     const handleChange=(e)=>{
         const {name,value}=e.target
         setFormData({...formData,[name]:value})
     }
     
-    const handleSubmit=(e)=>{
+    const handleSubmit=async(e)=>{
         e.preventDefault()
-        console.log(formData)
+        try{
+            const user=await axios.post("http://localhost:4096/auth/login",formData)
+            localStorage.setItem("token",user.data.token)
+            navigate("/")
+        }catch(e){
+            console.log(e)
+           setSereverErrors(e.response.data.errors)
+        }
+        
     }
+    
   return (
     <div>
     <h1>Login Form</h1>
+    {
+            serverErrors.length>0 && (
+                <div>
+                    <h3>These errors prevented form from submitting please correct these error</h3>
+                    <ul>
+                        {
+                            serverErrors.map((ele,i)=>{
+                                return <li key={i}><b>{ele.msg}</b></li>
+                            })
+                        }
+                    </ul>
+                </div>
+            )
+        }
     <p>{message}</p>
     <form onSubmit={handleSubmit}>
         <label>Enter email</label><br/>
